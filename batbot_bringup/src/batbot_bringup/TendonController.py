5f9f05b08b6b9535a17200e5bfc51acf6e9d4f47
jdv52 <jaysondv0928@gmail.com>
2024-11-08 07:31:58 +0000

    wrote some documentation

--- batbot_bringup/src/batbot_bringup/TendonController.py
+++ batbot_bringup/src/batbot_bringup/TendonController.py
@@ -17,7 +17,35 @@ class OPCODE(Enum):
 
 
 class TendonController:
+    '''
+    This class is used to control and interface with NEEC motor controller via a
+    serial connection. The relevant firmware for the motor controller is located
+    in the batbot_tendon_controller folder.
+
+    HOW TO USE
+    ==========
+    First, create a TendonController object specifying the communication
+    type and port number. Example:
+
+    `tendonController = TendonController(port_name="/dev/ttyACM0")`
+
+    You can then call any function to control a motor. Example:
+
+    `tendonController.writeMotorAngle(0, 120)`
+    
+    '''
+
+
     def __init__(self, com=COM_TYPE.NONE, port_name=''):
+        '''
+        Creates a tendon controller object.
+
+        The com argument specifies the serial interface that
+        the motor controller is connected by (SPI or UART).
+
+        The port name gives the serial port or device on which
+        the motor controller is connected (e.g. COM9 or /dev/ttyACM0)
+        '''
         self.th = TendonHardwareInterface(port_name)
             
 
@@ -27,6 +55,11 @@ class TendonController:
         raise NotImplementedError
 
     def writeMotorAngle(self, id, angle):
+        '''
+        This function sets the motor specified by id to move to angle.
+        The angle should be a signed integer between -360 to 360.
+        '''
+    
         # angle_h = (angle >> 8) & 0xFF
         # angle_l = angle & 0xFF
 
@@ -41,6 +74,10 @@ class TendonController:
         raise NotImplementedError
 
     def readMotorAngle(self, id):
+        '''
+        This function returns the angle of the motor specified by id.
+        '''
+
         # params = []
 
         # seq = ctypes.c_uint8 * len(params)
@@ -51,19 +88,32 @@ class TendonController:
 
         raise NotImplementedError
 
-    def moveMotorToMin(self):
+    def moveMotorToMin(self, id):
+        '''
+        This function moves the motor specified by id to its minimum angle
+        '''
         raise NotImplementedError
 
-    def moveMotorToMax(self):
+    def moveMotorToMax(self, id):
+        '''
+        This function moves the motor specified by id to its maximum angle
+        '''
         raise NotImplementedError
 
-    def moveMotorToZero(self):
+    def moveMotorToZero(self, id):
+        '''
+        This function moves the motor specified by id to 0
+        '''
         raise NotImplementedError
 
-    def setNewZero(self):
+    def setNewZero(self, id):
+        '''
+        For the motor specified by id, sets its current angle to the zero angle
+        '''
         raise NotImplementedError
 
 if __name__ == "__main__":
+
     import time
 
     tc = TendonController(port_name="/dev/ttyACM0")
